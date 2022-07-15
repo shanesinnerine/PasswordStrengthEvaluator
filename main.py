15505a54a877848d2ab696aef73e73718085c2f9
omgitsAlec <alecwebley03@gmail.com>
2022-07-15 01:15:26 -0400

    Added measurements for password

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-msg = "Hello World f"
-print(msg)
+name = input("Enter your name: ")
+dateOfBirth = input("Enter your date of birth")
+a = 1
+day = a
+month = a
+year = a
+password = input("Enter a password: ")
+length = len(password)
+capitalcount = list(filter(lambda c: c.isupper(), password))
+print(password)
+print(len(capitalcount))
+specialcharcount = a
+commonInputs = [] 
