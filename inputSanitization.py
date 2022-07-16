59d7f6748b865144096577e8b74a0cf7bdd3bbd4
Shane Sinnerine <shanesinnerine@gmail.com>
2022-07-16 01:06:05 -0400

    Added sanitization for inputs

---
+++ inputSanitization.py
@@ -0,0 +1,22 @@
+def cleanDoB(dateOfBirth):
+    dateOfBirth.replace(" ", "")
+    dobList = list(dateOfBirth.split("/"))
+    if (len(dobList) == 3 and len(dateOfBirth) == 10):
+        try:
+            day = int(dobList[0]) 
+            month = int(dobList[1])
+            year = int(dobList[2])
+        except:
+            print("Use Numbers dummy")
+            return False
+        if ((int(dobList[0]) == 2) and (int(dobList[1]) > 29)):
+            print("February is not that long bro...")
+            return False
+        elif ((0 < int(dobList[0]) <= 12) and (0 < int(dobList[1]) <= 31)):
+            return True
+    else:
+        print("Are you stupid enter a good DOB")
+    return False
+
+def cleanPW(password):
+    return password.__contains__(" ") or password.__contains__("\n")
--- main.py
+++ main.py
@@ -1,21 +1,23 @@
+import inputSanitization
+
+#Get user's name
 name = input("Enter your name: ")
 
-# Acquiring a Date of Birth from the User 
-dateOfBirth = input("Enter your date of birth MM/DD/YYYY: ")
-dateOfBirth.replace(" ", "")
-dobList = list(dateOfBirth.split("/"))
-day = int(dobList[0]) 
-month = int(dobList[1])
-year = int(dobList[2])
-print(day)
-print(month)
-print(year)
+# Get Date of Birth from the User
+while(True):
+    dateOfBirth = input("Enter your date of birth MM/DD/YYYY: ")
+    if(inputSanitization.cleanDoB(dateOfBirth)):
+        break
+
+#Get password from user
 while(True):
     password = input("Enter a password: ")
-    if(password.__contains__(" ") or password.__contains__("\n")):
+    if(inputSanitization.cleanPW(password)):
         print("Illegal input")
     else:
         break
+
+#Calculate length of password
 length = len(password)
 
 # Calculate number of capital letters in password
