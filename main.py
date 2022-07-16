6ef2e6fecd2497fd2d32c7f7021a4567e705f6a1
Shane Sinnerine <shanesinnerine@gmail.com>
2022-07-16 00:33:21 -0400

    Added special character count and date of birth entry

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 name = input("Enter your name: ")
-dateOfBirth = input("Enter your date of birth")
-a = 1
-day = a
-month = a
-year = a
-password = input("Enter a password: ")
+
+# Acquiring a Date of Birth from the User 
+dateOfBirth = input("Enter your date of birth MM/DD/YYYY: ")
+dateOfBirth.replace(" ", "")
+dobList = list(dateOfBirth.split("/"))
+day = int(dobList[0]) 
+month = int(dobList[1])
+year = int(dobList[2])
+print(day)
+print(month)
+print(year)
+while(True):
+    password = input("Enter a password: ")
+    if(password.__contains__(" ") or password.__contains__("\n")):
+        print("Illegal input")
+    else:
+        break
 length = len(password)
+
+# Calculate number of capital letters in password
 capitalcount = list(filter(lambda c: c.isupper(), password))
-print(password)
-print(len(capitalcount))
-specialcharcount = a
+
+#Calculate number of special characters
+specialcharcount = list(filter(lambda c: not c.isalnum(), password))
+
+#Test calculations
+print("Special count: " + str(len(specialcharcount)))
+print("Capital count: " + str(len(capitalcount)))
 commonInputs = [] 
